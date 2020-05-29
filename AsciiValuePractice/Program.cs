aa800523e06137ab3585248ad846bd83adafd9b5
lalit <lalit.mallick5692@gmail.com>
2020-05-29 17:29:30 +0530

    ascii value project

---
+++ AsciiValuePractice/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsciiValuePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* char c = '$';
+             int x = c;
+             Console.WriteLine(x);
+             Console.ReadLine();*/
+             //OR
+            string s;
+            Console.WriteLine("enter the sentence");
+            s = Console.ReadLine();
+            foreach (char c in s) 
+            {
+                Console.WriteLine((int)c);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
